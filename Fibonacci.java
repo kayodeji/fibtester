e8ab8ff35a622b098f6c8e9543c2b938bb66b11d
MrsAyodeji <kayodeji@theoakridgeschool.org>
2019-11-23 08:25:37 -0600

    Updated my solutions to the homefun by include the corrected tester file and the solution

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,33 +1,8 @@
 /** This is a solution to the Fibonacci HF writted by Mrs. Ayodeji */
 public class Fibonacci
 {
-/*	
-	public static void main(String[] args)
-	{
-		int[] ns = {12, 20, 29, 42};
-		int[] ks = {9, 13, 22, 39};
 
-		for (int i = 0; i< ns.length; i++)
-		{
-			int[] Fibarray = createFibonacci(ns[i]);
-			System.out.println("\nThe Fibonacci Array of " + ns[i] + " elements has been created");
-			System.out.println("\nThe first " + ks[i] + " elements of the Fibonacci Array is printed below");
-			printFibonacci(Fibarray, ks[i]);
-			double[] GRarray = goldenratio(Fibarray,ks[i]);
-			System.out.println("\nThe Ratio Array of " + (ks[i]-1) + " elements has been created");
-			System.out.println("\nThe first " + (ks[i]-1) + " elements of the Ratio Array is printed below");
-			for (int j=0; j< GRarray.length; j++)
-			{
-				System.out.println(GRarray[j]);
-			}
-			System.out.println("\nComparing the sum of the first " + ks[i] + " elements to the result of " + (ks[i]+2) + " - 1.");
-			System.out.print( " = " + compareSum(Fibarray, ks[i]));
-			System.out.println("\n\n\n");
-		}
-	}
-	*/
-	
-	/* Creates a array of the first n elements of the fibonacci sequence 
+	/* Creates a array of the first n elements of the fibonacci sequence
 	@param n the size of the array and the number of elements of the Fibonacci sequence
 	@result an array of the first n elements of the Fibonacci sequence
 	*/
@@ -38,15 +13,15 @@ public class Fibonacci
 			int[] arr = new int[n];
 			arr[0] = 1;
 			arr[1] = 1;
-			
+
 			for (int i = 2; i < n; i++)
-			{	
+			{
 				arr[i] = arr[i-1] + arr[i-2];
 			}
-			
+
 			return arr;
 		}
-		else 
+		else
 		{
 			int[] arr = {0};
 			return arr;
@@ -64,13 +39,13 @@ public class Fibonacci
 				System.out.print(a[i]+", ");
 			}
 		}
-		else 
+		else
 		{
 			System.out.println("The inputted " + k + " is larger than the size of the Fiboncci array.");
 		}
 	}
-	
-	/* Gives a ratio of consecutive Fibonacci numbers 
+
+	/* Gives a ratio of consecutive Fibonacci numbers
 	@param a an array of integers from the Fibonacci sequence
 	@param k the number of ratios to compute
 	@return an array of the results of k consecutive Fibonacci number ratios
@@ -94,7 +69,7 @@ public class Fibonacci
 			return empty;
 		}
 	}
-	
+
 	/* Compares the sum of the first k terms of the Fibonacci Sequence to f(k+2)-1
 	@param a an array of integers from the Fibonacci sequence
 	@param k the number of Fibonacci numbers to sum
@@ -109,7 +84,7 @@ public class Fibonacci
 			{
 				sum = sum + a[i];
 			}
-			
+
 			return a[k+2]-1 - sum;
 		}
 		else
@@ -117,8 +92,7 @@ public class Fibonacci
 			return -999;
 		}
 	}
-	
-	
-	
-}		
-			
+
+
+
+}
--- FibonacciTester.java
+++ FibonacciTester.java
@@ -1,36 +1,111 @@
-/* This is a tester program for the APCSA Quarter 2 Homefun Fibonacci 
+/* This is a tester program for the APCSA Quarter 2 Homefun Fibonacci
   * Created by Kymberly Ayodeji
   * November 2019
  */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class FibonacciTester
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
 		int[] ns = {12, 20, 29, 42};
 		int[] ks = {9, 13, 22, 39};
 
+		/* Take each students Fibonacci.java file location and add it to an array of Strings.
+		    Run the Tester code for each students paper and print the results to a file resuts.txt
+		*/
+
+
+
+		//Construct PrintWrite objects
+		String outputFile = "results.txt";
+		PrintWriter out = new PrintWriter(outputFile);
+		out.println(outputFile);
+		out.println("This is the results from running Mrs. Ayodeji's FibonacciTester on your Fibonacci homefun.\n");
+
 		for (int i = 0; i< ns.length; i++)
 		{
+			System.out.println("*********** Fibonacci Array of size n = "+ns[i] + " and k = " + ks[i]+". *******************");
+			out.println("*********** Fibonacci Array of size n ="+ns[i] + " and k = " + ks[i]+". *******************");
+			// Tests the students a) createFibonacci() method
 			int[] Fibarray = Fibonacci.createFibonacci(ns[i]);
-			System.out.println("\nThe Fibonacci Array of " + ns[i] + " elements has been created");
-			System.out.println("\nThe first " + ks[i] + " elements of the Fibonacci Array is printed below");
+			out.println("\nThe Fibonacci Array of " + ns[i] + " elements has been created");
+			out.println("\nThe first " + ks[i] + " elements of the Fibonacci Array is printed below");
+
+			//Prints the students' Fibonacci array to outputFile
+			for (int j=0; j< ks[i]; j++)
+			{
+					out.print(Fibarray[j] + ", ");
+			}
+			out.println("");
+			 // Tests the students b) printFibonacci() method
 			Fibonacci.printFibonacci(Fibarray, ks[i]);
+
+			// Tests the students c) goldenratio() method by printing to reulsts and finding the mean and sd
 			double[] GRarray = Fibonacci.goldenratio(Fibarray,ks[i]);
-			System.out.println("\nThe Ratio Array of " + (ks[i]-1) + " elements has been created");
-			System.out.println("\nThe first " +(ks[i]-1) + " elements of the Ratio Array is printed below");
+			out.println("\nThe Ratio Array of " + (ks[i]-1) + " elements has been created");
+			out.println("\nThe first " +(ks[i]) + " elements of the Ratio Array is printed below");
 			for (int j=0; j<GRarray.length; j++)
 			{
-				System.out.println(GRarray[j]);
+				out.println(GRarray[j]);
 			}
-			System.out.println("\nComparing the sum of the first " + ks[i] + " elements to the result of " +(ks[i]-1) + " - 1.");
-			System.out.print( " = " + Fibonacci.compareSum(Fibarray, ks[i]));
-			System.out.println("\n\n\n");
+			out.printf("\nFor the Ratio array, the mean is %7.4f", theMean(GRarray));
+			out.printf(" and the standard deviation is %7.4f", theSD(GRarray));
+			out.println(". ");
+
+			System.out.printf("\nFor the Ratio array, the mean is %7.4f", theMean(GRarray));
+			System.out.printf(" and the standard deviation is %7.4f", theSD(GRarray));
+			out.println(". ");
+
+				// Tests the students d) compareSum() method
+			out.print("\nComparing the sum of the first " + ks[i] + " elements to the result of F(" +(ks[i]+2) + ") - 1.  ");
+			out.print( "The difference is  = " + Fibonacci.compareSum(Fibarray, ks[i]));
+			out.println("\n\n\n");
+
+			System.out.print("\nComparing the sum of the first " + ks[i] + " elements to the result of F(" +(ks[i]+2) + ") - 1. ");
+			System.out.print( "The difference is  = " + Fibonacci.compareSum(Fibarray, ks[i]));
+			System.out.println("\n");
 		}
+			out.println("\n\n\n");
+			out.close();
 	}
-	
+
+		public static double theMean(double[] arr)
+		{
+			double sum = 0;
+			for (double entry : arr)
+			{
+					sum = sum + entry;
+			}
+			if (arr.length > 0)
+				{
+					return sum/arr.length;
+				}
+			else
+			{
+					return -99999;
+			}
+		}
+
+		public static double theSD(double[] arr)
+		{
+				if (arr.length > 0)
+				{
+						double xbar = theMean(arr);
+						double sum = 0;
+						for (double entry : arr)
+						{
+								double diffsquared = Math.pow((xbar-entry),2);
+								sum = sum + diffsquared;
+							}
+							return Math.pow(sum/(arr.length-1),0.5);
+					}
+				else
+				{
+						return -9999;
+				}
+		}
+
 }
-			
-		     
-			
-		
